3eb2565cb40f4b64e6e66a08392f3cd434cf4306
Junayd Bhoyroo <hello@junaydbhoyroo.com>
2023-09-24 02:26:16 +0100

    upload, edit, and download edited json (no validation)

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-const table = document.getElementById("data");
-const saveButton = document.getElementById("save");
+let filename;
+const fileUploadButton = document.getElementById("upload");
+const downloadButton = document.getElementById("download");
+const textarea = document.querySelector("textarea");
 
-async function getPetData() {
-  const raw = await fetch("./data.json");
-  const parsed = await raw.json();
+async function handleUpload({ target }) {
+  const file = target.files[0];
+  filename = file.name.replace(".json", "");
+  const jsonString = await file.text();
 
-  return parsed.petData;
+  displayJSON(JSON.parse(JSON.stringify(jsonString)));
 }
 
-async function buildTable() {
-  const petData = await getPetData();
+function displayJSON(jsonString) {
+  const { rows, cols } = getTextAreaDimensions(jsonString);
+  textarea.rows = rows;
+  textarea.cols = cols;
+  textarea.innerHTML = jsonString;
+}
 
-  console.log(petData);
-
-  petData.forEach((pet, i) => {
-    const row = table.insertRow(-1);
-    row.setAttribute("class", "data-row");
-
-    for (const [key, value] of Object.entries(pet)) {
-      const cell = row.insertCell(-1);
-      const input = document.createElement("input");
-
-      input.setAttribute("type", "text");
-      input.setAttribute("value", value);
-      cell.appendChild(input);
-    }
+function downloadJSON({ target }) {
+  const blob = new Blob(textarea.value.split(""), {
+    type: "application/json",
   });
+  const blobURL = URL.createObjectURL(blob);
+
+  target.href = blobURL;
+  target.download = `${filename}-edited.json`;
 }
 
-function buildJSON() {
-  const allData = document.querySelectorAll(".data-row");
-  // TO-DO: iterate over each child of data row and build JSON Blob
+function getTextAreaDimensions(str) {
+  const lines = str.split("\n");
+  const rows = lines.length - 1;
+  const columns = Math.max(...lines.map((line) => line.length)) - 1;
+
+  return { rows: rows, cols: columns };
 }
 
-saveButton.addEventListener("click", buildJSON);
-
-buildTable();
+fileUploadButton.addEventListener("change", handleUpload);
+downloadButton.addEventListener("click", downloadJSON);
